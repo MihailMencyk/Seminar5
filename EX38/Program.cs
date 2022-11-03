3a40308d27ebb4b0f24171c59b6c1d1afee81586
Mihail Mencyk <m.mencyk@gmail.com>
2022-11-04 02:32:24 +0300

    EX38

---
+++ EX38/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным 
+// элементов массива. [3 7 22 2 78] -> 76
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 100);
+        System.Console.Write($"{array[i]} ");
+    }
+System.Console.WriteLine();
+}
+int[] array = new int [4];
+PrintArray (array);
+int min = array[0];
+int max = array[0];
+for (int i = 1; i < array.Length; i++)
+{
+    if (min>array[i]) min = array[i];
+}
+for (int i = 1; i < array.Length; i++)
+{
+    if (max<array[i]) max = array[i];
+}
+System.Console.WriteLine($"максимальное: {max}, минимальное: {min}, Разница чисел: {max-min}");
